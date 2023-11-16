f9399c95e59cdd8ba682e0c255893cb6183bf7e9
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-16 20:25:54 +0100

    Donation entity added.

---
+++ src/main/java/pl/coderslab/charity/entities/Donation.java
@@ -0,0 +1,31 @@
+package pl.coderslab.charity.entities;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity; // amount of 60l bags in donation
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Category> categories;
+    @ManyToOne
+    private Institution institution;
+    private String street;
+    private String city;
+    @Column(name = "zip_code")
+    private String zipCode;
+    @Column(name = "pick_up_date")
+    private LocalDate pickUpDate;
+    @Column(name = "pick_up_time")
+    private LocalTime pickUpTime;
+    @Column(name = "pick_up_comment")
+    private String pickUpComment;
+}
