931102f548c153b1e42f3d2d147763121e000930
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-16 21:32:00 +0100

    DonationRepository added Query for all bags amount.

--- src/main/java/pl/coderslab/charity/repositories/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repositories/DonationRepository.java
@@ -1,7 +1,11 @@
 package pl.coderslab.charity.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.entities.Donation;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
+
+    @Query("SELECT SUM(d.quantity) FROM Donation d")
+    Integer getAmountOfBags();
 }
