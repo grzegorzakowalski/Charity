86f1b8b74f4de182e20eb554fde1aba84ee3d4a5
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-16 21:07:14 +0100

    InstitutionServiceTests added for getRandomFour.

---
+++ src/test/java/pl/coderslab/charity/services/InstitutionServiceTests.java
@@ -0,0 +1,56 @@
+package pl.coderslab.charity.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.coderslab.charity.entities.Institution;
+import pl.coderslab.charity.repositories.InstitutionRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class InstitutionServiceTests {
+    @Mock
+    private InstitutionRepository institutionRepository;
+
+    @InjectMocks
+    private InstitutionService institutionService;
+
+    @Test
+    public void whenDataBaseIsEmpty_getRandomFour_ShouldReturnEmptyList(){
+        Mockito.when(institutionRepository.findAll()).thenReturn(new ArrayList<>());
+        List<Institution> actual = institutionService.getRandomFour();
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void whenInDataBaseIsLessThenFifeRows_getRandomFour_ShouldGiveAll(){
+        List<Institution> expected = new ArrayList<>();
+        expected.add(new Institution());
+        expected.add(new Institution());
+        expected.add(new Institution());
+        Mockito.when(institutionRepository.findAll()).thenReturn(expected);
+        List<Institution> actual = institutionService.getRandomFour();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenInDataBaseIsMoreThenFour_getRandomFour_ShouldGiveFour(){
+        List<Institution> dataBase = new ArrayList<>();
+        dataBase.add(new Institution());
+        dataBase.add(new Institution());
+        dataBase.add(new Institution());
+        dataBase.add(new Institution());
+        dataBase.add(new Institution());
+        Mockito.when(institutionRepository.findAll()).thenReturn(dataBase);
+        List<Institution> actual = institutionService.getRandomFour();
+        assertEquals(4, actual.size());
+    }
+
+}
