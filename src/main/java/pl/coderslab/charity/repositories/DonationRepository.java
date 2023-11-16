04925ca545de864fbabfd06ac6bcb85aae3022ca
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-16 21:43:19 +0100

    DonationRepository getAmountOfDonations added.

--- src/main/java/pl/coderslab/charity/repositories/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repositories/DonationRepository.java
@@ -6,6 +6,15 @@ import pl.coderslab.charity.entities.Donation;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
 
+    /**
+     * @return Amount of bags or null if db is empty.
+     */
     @Query("SELECT SUM(d.quantity) FROM Donation d")
     Integer getAmountOfBags();
+
+    /**
+     * @return Amount of donations.
+     */
+    @Query("SELECT COUNT(d) FROM Donation d")
+    Integer getAmountOfDonations();
 }
