9668f2c36b92e96e5c0e4afeb55e35aa0a182402
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-16 21:32:29 +0100

    Added dynamic bags amount to home view.

--- src/main/java/pl/coderslab/charity/controllers/HomeController.java
+++ src/main/java/pl/coderslab/charity/controllers/HomeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.repositories.DonationRepository;
 import pl.coderslab.charity.services.InstitutionService;
 
 
@@ -12,10 +13,13 @@ import pl.coderslab.charity.services.InstitutionService;
 public class HomeController {
 
     private final InstitutionService institutionService;
+    private final DonationRepository donationRepository;
 
     @RequestMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutionList", institutionService.getRandomFour());
+        Integer bagsAmount = donationRepository.getAmountOfBags();
+        model.addAttribute("bagsAmount",bagsAmount == null? 0 : bagsAmount);
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -19,8 +19,7 @@
 <section class="stats">
     <div class="container container--85">
         <div class="stats--item">
-            <em>13</em>
-
+            <em>${bagsAmount}</em>
             <h3>Oddanych worków</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius est beatae, quod accusamus illum
                 tempora!</p>
