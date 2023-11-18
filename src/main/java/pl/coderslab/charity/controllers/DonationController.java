e8d1eace2dace2f7f19cbd2bc4f1831d27962ade
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-18 16:20:22 +0100

    Form third step completed.

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import pl.coderslab.charity.CategoryRepository;
 import pl.coderslab.charity.entities.Donation;
 import pl.coderslab.charity.repositories.DonationRepository;
+import pl.coderslab.charity.repositories.InstitutionRepository;
 
 @Controller
 @RequiredArgsConstructor
 public class DonationController {
     private final DonationRepository donationRepository;
     private final CategoryRepository categoryRepository;
+    private final InstitutionRepository institutionRepository;
 
     @GetMapping("/form")
     public String donationFormView(Model model){
         model.addAttribute("donation", new Donation());
         model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("institutions",institutionRepository.findAll());
         return "form";
     }
 }
