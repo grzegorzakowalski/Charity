de4899410d3cfcb17aaa6a81da2b1c4e153a81b1
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-18 21:39:54 +0100

    Form complete.

--- src/main/webapp/WEB-INF/views/form.jsp
+++ src/main/webapp/WEB-INF/views/form.jsp
@@ -173,16 +173,12 @@
                         <ul>
                             <li>
                                 <span class="icon icon-bag"></span>
-                                <span class="summary--text"
-                                >4 worki ubrań w dobrym stanie dla dzieci</span
-                                >
+                                <span class="summary--text">4 worki ubrań w dobrym stanie dla dzieci</span>
                             </li>
 
                             <li>
                                 <span class="icon icon-hand"></span>
-                                <span class="summary--text"
-                                >Dla fundacji "Mam marzenie" w Warszawie</span
-                                >
+                                <span class="summary--text">Dla fundacji "Mam marzenie" w Warszawie</span                                >
                             </li>
                         </ul>
                     </div>
@@ -191,19 +187,19 @@
                         <div class="form-section--column">
                             <h4>Adres odbioru:</h4>
                             <ul>
-                                <li>Prosta 51</li>
-                                <li>Warszawa</li>
-                                <li>99-098</li>
-                                <li>123 456 789</li>
+<%--                                <li>Prosta 51</li>--%>
+<%--                                <li>Warszawa</li>--%>
+<%--                                <li>99-098</li>--%>
+<%--                                <li>123 456 789</li>--%>
                             </ul>
                         </div>
 
                         <div class="form-section--column">
                             <h4>Termin odbioru:</h4>
                             <ul>
-                                <li>13/12/2018</li>
-                                <li>15:40</li>
-                                <li>Brak uwag</li>
+<%--                                <li>13/12/2018</li>--%>
+<%--                                <li>15:40</li>--%>
+<%--                                <li>Brak uwag</li>--%>
                             </ul>
                         </div>
                     </div>
--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -165,6 +165,59 @@ document.addEventListener("DOMContentLoaded", function() {
       this.$step.parentElement.hidden = this.currentStep >= 5;
 
       // TODO: get data from inputs and show them in summary
+      // finding all data to put into summary
+      const categories = document.querySelectorAll('[name="categories"]:checked');
+      const bags = document.querySelector('#quantity');
+      const foundation = document.querySelector('[name="institution"]:checked');
+      const street = document.querySelector('[name="street"]');
+      const city = document.querySelector('[name="city"]');
+      const zipCode = document.querySelector('[name="zipCode"]');
+      const phoneNumber = document.querySelector('[name="phoneNumber"]');
+      const pickUpDate = document.querySelector('[name="pickUpDate"]');
+      const pickUpTime = document.querySelector('[name="pickUpTime"]');
+      const pickUpComment = document.querySelector('[name="pickUpComment"]');
+
+      // finding all fields in summary
+      const summaryText = document.querySelectorAll('.summary--text');
+      const formSectionColumn = document.querySelectorAll('.form-section--column');
+
+      // setting up values into summary
+      if( bags.value > 1){
+        summaryText.item(0).innerText = bags.value + " worki ";
+      } else {
+        summaryText.item(0).innerText = bags.value + " worek ";
+      }
+      categories.forEach(category => {
+        summaryText.item(0).innerText += " " + category.parentElement.querySelector(".description").innerText;
+      });
+      summaryText.item(1).innerText = "Dla fundacji " + foundation.parentElement.querySelector('.title').innerText;
+
+      const address = [document.createElement('li'),document.createElement('li'),document.createElement('li'),document.createElement('li')]
+      address.at(0).innerText = street.value;
+      address.at(1).innerText = city.value;
+      address.at(2).innerText = zipCode.value;
+      address.at(3).innerText = phoneNumber.value;
+      const h4 = document.createElement('h4');
+      h4.innerText = 'Adres odbioru:';
+      const ul = document.createElement('ul');
+
+      formSectionColumn.item(2).innerHTML = '';
+      formSectionColumn.item(2).appendChild(h4);
+      address.forEach(el => ul.appendChild(el));
+      formSectionColumn.item(2).appendChild(ul);
+
+      const pickUp = [document.createElement('li'),document.createElement('li'),document.createElement('li')];
+      pickUp[0].innerText = pickUpDate.value;
+      pickUp[1].innerText = pickUpTime.value;
+      pickUp[2].innerText = pickUpComment.value;
+      const h42 = document.createElement('h4');
+      h42.innerText = 'Termin odbioru:';
+      const ul2 = document.createElement('ul');
+      formSectionColumn.item(3).innerHTML = '';
+      formSectionColumn.item(3).appendChild(h42);
+      pickUp.forEach(el => ul2.appendChild(el));
+      formSectionColumn.item(3).appendChild(ul2);
+
     }
 
   }
