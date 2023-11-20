0dd8632858af9b29d935822325f017e01362407d
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-20 21:06:53 +0100

    Fix to registration.

--- src/main/java/pl/coderslab/charity/controllers/RegisterController.java
+++ src/main/java/pl/coderslab/charity/controllers/RegisterController.java
@@ -1,6 +1,7 @@
 package pl.coderslab.charity.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import pl.coderslab.charity.entities.User;
 import pl.coderslab.charity.repositories.UserRepository;
 
+import java.util.ArrayList;
+
 @Controller
 @RequiredArgsConstructor
 public class RegisterController {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @GetMapping("/register")
     public String registrationFormView(Model model, @RequestParam(required = false, name = "exist") String exist){
         model.addAttribute("user", new User());
@@ -23,6 +27,9 @@ public class RegisterController {
     @PostMapping("/register")
     public String addUser(User user){
         if( userRepository.findUserByUsername(user.getUsername()) == null){
+            user.setRole("ROLE_USER");
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setDonations(new ArrayList<>());
             userRepository.save(user);
             return "redirect:/";
         }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -2,6 +2,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -61,7 +62,7 @@
         </div>
     </div>
 
-    <a href="<c:url value="/register"/>" class="btn btn--large">Załóż konto</a>
+    <sec:authorize access="isAnonymous()"> <a href="<c:url value="/register"/>" class="btn btn--large">Załóż konto</a> </sec:authorize>
 </section>
 
 <section class="about-us">
