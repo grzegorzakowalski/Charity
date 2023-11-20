86786fbc8e7159f03390e0914155558c28ac0096
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-20 22:08:50 +0100

    Constructor added.

--- src/main/java/pl/coderslab/charity/pojo/InstitutionPair.java
+++ src/main/java/pl/coderslab/charity/pojo/InstitutionPair.java
@@ -3,9 +3,14 @@ package pl.coderslab.charity.pojo;
 import lombok.Data;
 import pl.coderslab.charity.entities.Institution;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class InstitutionPair {
     private List<Institution> institutionPair;
+
+    public InstitutionPair(){
+        institutionPair = new ArrayList<>();
+    }
 }
