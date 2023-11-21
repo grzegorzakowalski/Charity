04d6040cf5618552ba75cd5c3d80e4567e804035
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-21 21:45:31 +0100

    Admin crud added.

--- src/main/java/pl/coderslab/charity/controllers/PanelController.java
+++ src/main/java/pl/coderslab/charity/controllers/PanelController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.coderslab.charity.CategoryRepository;
 import pl.coderslab.charity.entities.User;
+import pl.coderslab.charity.repositories.DonationRepository;
+import pl.coderslab.charity.repositories.InstitutionRepository;
 import pl.coderslab.charity.repositories.UserRepository;
 import pl.coderslab.charity.security.CurrentUser;
 import pl.coderslab.charity.services.UserService;
@@ -21,9 +24,12 @@ public class PanelController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final DonationRepository donationRepository;
+    private final InstitutionRepository institutionRepository;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping("/user")
-    public String userUserPanelView(@AuthenticationPrincipal CurrentUser currentUser, Model model){
+    public String userPanelView(@AuthenticationPrincipal CurrentUser currentUser, Model model){
         model.addAttribute("user",currentUser.getUser());
         model.addAttribute("bags",userService.getUserBagsDonatedAmount(currentUser.getUser()));
         model.addAttribute("donations",userService.getUserDonationsAmount(currentUser.getUser()));
@@ -43,5 +49,11 @@ public class PanelController {
         user.setPassword(passwordEncoder.encode(password));
         userRepository.save(user);
         return "redirect:/panel/user";
+    }
+
+    @GetMapping("/crud")
+    public String adminCrudView(Model model){
+
+        return "panel-crud";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/panel-crud.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kowal
+  Date: 21.11.2023
+  Time: 21:03
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
