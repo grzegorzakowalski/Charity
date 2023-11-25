65f6bbbe2fe19d8b24bc97de79c01fecd69ea774
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-25 13:48:05 +0100

    User modification added.

--- src/main/java/pl/coderslab/charity/controllers/PanelController.java
+++ src/main/java/pl/coderslab/charity/controllers/PanelController.java
@@ -18,11 +18,10 @@ import pl.coderslab.charity.repositories.UserRepository;
 import pl.coderslab.charity.security.CurrentUser;
 import pl.coderslab.charity.services.UserService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -34,6 +33,7 @@ public class PanelController {
     private final DonationRepository donationRepository;
     private final InstitutionRepository institutionRepository;
     private final CategoryRepository categoryRepository;
+    private final List<String> roles = Stream.of("ROLE_ADMIN","ROLE_USER").toList();
 
     @GetMapping("/user")
     public String userPanelView(@AuthenticationPrincipal CurrentUser currentUser, Model model){
@@ -60,32 +60,56 @@ public class PanelController {
 
     @GetMapping("/crud")
     public String adminCrudView(Model model, @AuthenticationPrincipal CurrentUser currentUser,
-                                @RequestParam(name = "msg", required = false) String msg){
+                                @RequestParam(name = "msg", required = false) String msg,
+                                @RequestParam(name = "error", required = false) String error){
         model.addAttribute("users", userService.getAllWithout(currentUser.getUser()));
         model.addAttribute("donations", donationRepository.findAll());
         model.addAttribute("institutions", institutionRepository.findAll());
         model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("msg", msg != null ? msg.replaceAll("_", " ") : null);
+        model.addAttribute("error", error != null ? error.replaceAll("_", " ") : null);
         return "panel-crud";
     }
 
     @GetMapping("/user/add")
     public String addUserView(Model model, @RequestParam(name = "exist", required = false) String exist){
         model.addAttribute("user", new User());
-        model.addAttribute("roles", Stream.of("ROLE_ADMIN","ROLE_USER").toList());
+        model.addAttribute("roles", roles);
         model.addAttribute("exist", exist);
         return "panel-user-add";
     }
 
-    @PostMapping("/user/add") // TODO fix relogging bug
+    @PostMapping("/user/add")
     public String addUser(User user){
         if( userRepository.findUserByUsername(user.getUsername()) != null){
             return "redirect:/panel/user/add?exist=true";
         }
-        user.setId(null);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return "redirect:/panel/crud?msg=user_added";
+    }
+
+    @GetMapping("/user/modify")
+    public String modifyUserView(Model model, @RequestParam(name = "id") long id){
+        model.addAttribute("user", userRepository.findById(id).orElse(null));
+        model.addAttribute("roles", roles);
+        return "panel-user-modify";
+    }
+
+    @PostMapping("/user/modify")
+    public String modifyUser(User user){
+        Optional<User> userFromDb = userRepository.findById(user.getId());
+        User oldUser = userFromDb.orElse(null);
+        if( oldUser == null){
+            return "redirect:/panel/crud?error=user_not_found";
+        }
+        if(user.getPassword().isEmpty()){
+            user.setPassword(oldUser.getPassword());
+        } else {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+        userRepository.save(user);
+        return "redirect:/panel/crud?msg=user_modify";
     }
 
     @GetMapping("/institution/add")
---
+++ src/main/webapp/WEB-INF/views/panel-user-modify.jsp
@@ -0,0 +1,64 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: kowal
+  Date: 25.11.2023
+  Time: 13:06
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+    <link rel="stylesheet" href="<c:url value="/resources/css/style.css"/>" />
+</head>
+<body>
+<header>
+<c:import url="nav.jsp"/>
+</header>
+
+<section class="login-page">
+    <c:if test="${user == null}">
+        <h2>Coś poszło nie tak!</h2>
+        <a href="<c:url value="/panel/crud"/>"><button class="btn" type="button">Wróć</button> </a>
+    </c:if>
+    <c:if test="${user != null}">
+    <h2>Zmodyfikuj użytkownika</h2>
+    <form:form method="post" modelAttribute="user">
+        <div class="form-group">
+            Login:<br>
+            <form:input path="username"/>
+        </div>
+        <div class="form-group">
+            Hasło:<br>
+            <form:password path="password" />
+        </div>
+        <div class="form-group">
+            Imię:<br>
+            <form:input path="firstName"/>
+        </div>
+        <div class="form-group">
+            Nazwisko:<br>
+            <form:input path="lastName"/>
+        </div>
+        <div class="form-group">
+            Rola:<br>
+            <form:select path="role">
+                <form:options items="${roles}"/>
+            </form:select>
+        </div>
+        <form:hidden path="id"/>
+        <form:hidden path="donations"/>
+        <div class="form-group form-group--buttons">
+            <button class="btn" type="submit">Zmodyfikuj użytkownika</button>
+        </div>
+    </form:form>
+    </c:if>
+</section>
+<c:import url="footer.jsp"/>
+</body>
+</html>
