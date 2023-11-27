61df97fa2ffa040e2488b84177e537cf07238d78
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-27 22:03:05 +0100

    Display name method change.

--- src/main/java/pl/coderslab/charity/security/CurrentUser.java
+++ src/main/java/pl/coderslab/charity/security/CurrentUser.java
@@ -10,8 +10,10 @@ import java.util.Collection;
 public class CurrentUser extends User {
 
     private final pl.coderslab.charity.entities.User user;
+    private final String firstName;
     public CurrentUser(String username, String password, Collection<? extends GrantedAuthority> authorities, pl.coderslab.charity.entities.User user) {
         super(username, password, user.isActive(), true, true, true,  authorities);
+        firstName = user.getFirstName();
         this.user = user;
     }
 }
