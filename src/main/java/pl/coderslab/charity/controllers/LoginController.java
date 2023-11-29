160050fd4873271a546cd004bfc8a138eed58ab9
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-11-29 20:47:41 +0100

    Moved functionality.

--- src/main/java/pl/coderslab/charity/controllers/LoginController.java
+++ src/main/java/pl/coderslab/charity/controllers/LoginController.java
@@ -16,8 +16,6 @@ import java.util.UUID;
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
-    private final UserRepository userRepository;
-    private final EmailServiceImpl emailService;
 
     @GetMapping("/login")
     public String loginView(Model model){
@@ -25,24 +23,4 @@ public class LoginController {
         return "login";
     }
 
-    @GetMapping("/password-renew")
-    public String forgotPasswordView(Model model, @RequestParam(name = "msg", required = false)String msg){
-        model.addAttribute("msg", msg != null ? msg.replaceAll("_", " ") : null);
-        return "password-renew";
-    }
-
-    @PostMapping("/password-renew")
-    public String forgotPassword(@RequestParam(name = "email", required = false)String email,
-                                 HttpServletRequest request){
-        if(email == null || email.isEmpty()){
-            return "redirect:/password-renew?msg=podaj_adres_email";
-        }
-        User user = userRepository.findUserByUsername(email);
-        if( user == null){
-            return "redirect:/password-renew?msg=Nie_ma_takiego_konta";
-        }
-        user.setUUID(UUID.randomUUID().toString());
-        emailService.sendSimpleMessage(email, "Resetowanie hasła","Kliknij w ten link aby przejść do formularza resetowania hasła: " + request.getRequestURL() + "?UUID=" + user.getUUID());
-        return "redirect:/password-renew?msg=Wysłano_email_z_linkiem_do_zresetowania_hasła";
-    }
 }
