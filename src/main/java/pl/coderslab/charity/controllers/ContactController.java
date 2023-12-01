0c6b8938f1218c28a2143033210512c5babb93a4
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-12-01 21:14:59 +0100

    Active to model added.

--- src/main/java/pl/coderslab/charity/controllers/ContactController.java
+++ src/main/java/pl/coderslab/charity/controllers/ContactController.java
@@ -2,6 +2,7 @@ package pl.coderslab.charity.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,8 @@ import pl.coderslab.charity.repositories.ContactMSGRepository;
 public class ContactController {
     private final ContactMSGRepository contactMSGRepository;
     @GetMapping
-    public String contactView(){
+    public String contactView(Model model){
+        model.addAttribute("active","contact");
         return "contact";
     }
 
