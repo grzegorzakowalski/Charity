7fb12a52ebbeaa49f036b9ca845da5b4a63704ee
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-12-01 20:30:41 +0100

    contactMSGHandler added.

--- src/main/java/pl/coderslab/charity/controllers/ContactController.java
+++ src/main/java/pl/coderslab/charity/controllers/ContactController.java
@@ -1,14 +1,26 @@
 package pl.coderslab.charity.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entities.ContactMSG;
+import pl.coderslab.charity.repositories.ContactMSGRepository;
 
 @Controller
 @RequestMapping("/contact")
+@RequiredArgsConstructor
 public class ContactController {
+    private final ContactMSGRepository contactMSGRepository;
     @GetMapping
     public String contactView(){
         return "contact";
+    }
+
+    @PostMapping
+    public String contactMSGHandler(ContactMSG msg){
+        contactMSGRepository.save(msg);
+        return "redirect:/";
     }
 }
